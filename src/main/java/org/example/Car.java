157a9e5b2b47c8e06493752cd65a23967d289cdc
깃hwjk16322 <hwjk16322@gmail.com>
2023-09-20 10:16:40 +0900

    0920 1016

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Car {
+
+
+    String name;
+    int speed;
+    String brand;
+    int carNumber;
+
+    void run(){
+        System.out.println(this.carNumber + " 자동차가 "+ this.speed +"km/h 로 달립니다..");
+    }
+
+}
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,47 @@
 package org.example;
 
-import java.util.ArrayList;
+/* class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        StringBuilder str1_1 = new StringBuilder(str1);
+        StringBuilder str2_1 = new StringBuilder(str2);
 
-// Shift을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
-// Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
-class Solution {
-    public String solution(String my_string) {
-        String answer = "";
+        StringBuilder answer_1 = str1_1.replace(str2, "*");
+        int count = 0;
+        for (int i = 0; i < answer_1.length(); i++) {
+            if (answer_1[i] == "*") {
+                count++;
 
-        char[] a = my_string.toCharArray();
 
+            }
+        }
+        if (count >= 1) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
         return answer;
     }
-
 }
+*/
 
+class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+        Car c3 = new Car();
 
+        c1.carNumber = 1234;
+        c2.carNumber = 2345;
+        c3.carNumber = 3456;
+
+        c1.speed = 200;
+        c2.speed = 300;
+        c3.speed = 150;
+
+        c1.run();
+        c2.run();
+        c3.run();
+
+    }
+}
