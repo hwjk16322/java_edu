a217053d9941f6e3f38ac3ecf8c6c52913c40aff
깃hwjk16322 <hwjk16322@gmail.com>
2023-09-25 13:20:10 +0900

    0925 1320

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -740,7 +740,7 @@ class 병사 {
     int 계급번호 = 0;
     String 계급 = 계급표[계급번호];
 
-    int 공격력;
+    int 공격력 = 7;
 
     void 자기소개() {
         System.out.println(String.format("안녕하세요. 저는 %s %s 입니다.", this.이름, this.계급));
@@ -748,7 +748,7 @@ class 병사 {
 
 
     void 공격() {
-        System.out.println(String.format("%s %s 이 공격합니다.(공격력 : %d)", this.이름, this.계급, this.공격력));
+        System.out.println(String.format("%s %s이(가) 공격합니다.(공격력 : %d)", this.이름, this.계급, this.공격력));
     }
 
     void 진급() {
@@ -757,13 +757,18 @@ class 병사 {
         } else {
             this.계급번호++;
             this.계급 = 계급표[계급번호];
+            this.공격력 = this.공격력+2;
         }
     }
 }
 
 
+
 */
 
+
+
+/*
 class Main {
     public static void main(String[] args) {
         전사 a전사 = new 홍길동();
@@ -840,4 +845,4 @@ class 칼 extends 무기 {
     }
 
 }
-
+*/
